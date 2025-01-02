432152ab92f872ab8443c84c23a0e2b825200b11
Matthew Wishek <matthew@wishek.com>
2025-01-02 00:02:07 -0700

    rework bit widths and refactor code

--- src/lowpass_ema.vhd
+++ src/lowpass_ema.vhd
@@ -59,6 +59,7 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
+USE ieee.math_real.ALL;
 
 
 ------------------------------------------------------------------------------------------------------
@@ -70,20 +71,22 @@ USE ieee.numeric_std.ALL;
 
 ENTITY lowpass_ema IS 
 	GENERIC (
-		ALPHA_W 		: NATURAL := 8;
-		DATA_W 			: NATURAL := 16;
-		AVG_W 			: NATURAL := 16
+		ALPHA_W 		: NATURAL := 18;
+		DATA_W 			: NATURAL := 23;
+		MULT_A_W		: NATURAL := 25;
+		MULT_B_W 		: NATURAL := 18;
+		PROD_W			: NATURAL := MULT_A_W + MULT_B_W
 	);
 	PORT (
 		clk 				: IN  std_logic;
 		init 				: IN  std_logic;
 
-		alpha 				: IN  std_logic_vector(ALPHA_W -1 DOWNTO 0);
+		alpha 				: IN  std_logic_vector(ALPHA_W -1 DOWNTO 0); -- u0.ALPHA_W
 
-		data 				: IN  std_logic_vector(DATA_W -1 DOWNTO 0);
+		data 				: IN  std_logic_vector(DATA_W -1 DOWNTO 0);  -- uDATA_W.0
 		data_ena 			: IN  std_logic;
 
-		average 			: OUT std_logic_vector(DATA_W -1 DOWNTO 0);
+		average 			: OUT std_logic_vector(DATA_W -1 DOWNTO 0);  -- uDATA_W.0
 		average_ena 		: OUT std_logic
 	);
 END ENTITY lowpass_ema;
@@ -97,36 +100,71 @@ END ENTITY lowpass_ema;
 
 ARCHITECTURE rtl OF lowpass_ema IS 
 
-	CONSTANT SHIFT_IN 		: NATURAL := AVG_W - DATA_W;
+	CONSTANT alpha_max		: signed(ALPHA_W -1 DOWNTO 0) := to_signed(2**(ALPHA_W-1) -1, ALPHA_W);
 
-	CONSTANT alpha_max		: unsigned(ALPHA_W -1 DOWNTO 0) := to_unsigned(2**ALPHA_W -1, ALPHA_W);
+	SIGNAL data_signed		: signed(DATA_W -1 DOWNTO 0);
 
-	SIGNAL data_r 			: signed(AVG_W -1 DOWNTO 0);
-	SIGNAL alpha_r			: unsigned(AVG_W -1 DOWNTO 0);
-	SIGNAL alpha_m 			: unsigned(AVG_W -1 DOWNTO 0);
-	SIGNAL ema				: signed(AVG_W -1 DOWNTO 0);
+	SIGNAL alpha_signed		: signed(ALPHA_W -1 DOWNTO 0);
+	SIGNAL alpha_m 			: signed(ALPHA_W -1 DOWNTO 0);
+
+	CONSTANT SUM_SHIFT 		: NATURAL := PROD_W - MULT_A_W;
+	SIGNAL sum				: signed(PROD_W -1 DOWNTO 0);
+	SIGNAL sum_r			: signed(PROD_W - SUM_SHIFT -1 DOWNTO 0);
+
+	CONSTANT MULT_DATA_SHIFT: NATURAL := PROD_W - ALPHA_W - DATA_W + 1;
+	SIGNAL mult_data 		: signed(PROD_W -1 DOWNTO 0);
+
+	CONSTANT MULT_SUM_SHIFT	: NATURAL := SUM_SHIFT - (ALPHA_W -1);
+	SIGNAL mult_sum 		: signed(PROD_W -1 DOWNTO 0);
+
+	CONSTANT AVG_SHIFT 		: NATURAL := PROD_W - DATA_W;
+
+	-- Debug stuff
+
+	CONSTANT FULL_SCALE 	: REAL := real(2**((DATA_W+1)/2-1)-1);
+
+	SIGNAL avg_rms 			: REAL := 0.0;
+	SIGNAL avg_real 		: REAL := 0.0;
+
 
 BEGIN 
 
--- truncate and round
-average <= std_logic_vector(resize(shift_right(shift_right(ema, ALPHA_W-1)+1, 1), DATA_W));
+-- pragma translate_off
+ASSERT False REPORT "PROD_W: " & integer'image(PROD_W) SEVERITY NOTE;
+ASSERT False REPORT "SUM_SHIFT: " & integer'image(SUM_SHIFT) SEVERITY NOTE;
+ASSERT False REPORT "MULT_DATA_SHIFT: " & integer'image(MULT_DATA_SHIFT) SEVERITY NOTE;
+ASSERT False REPORT "MULT_SUM_SHIFT: " & integer'image(MULT_SUM_SHIFT) SEVERITY NOTE;
+ASSERT False REPORT "AVG_SHIFT: " & integer'image(AVG_SHIFT) SEVERITY NOTE;
+ASSERT False REPORT "FULL_SCALE: " & real'image(FULL_SCALE) SEVERITY NOTE;
+--ASSERT False REPORT "AVG_REAL: " & real'image(avg_real) SEVERITY NOTE;
+
+avg_real <= SQRT(real(integer(to_integer(shift_right(sum, AVG_SHIFT)))));
+avg_rms <= 20.0*LOG10(avg_real/FULL_SCALE) WHEN avg_real > 0.0 ELSE 0.0;
+-- pragma translate_on
+
+average 	<= std_logic_vector(resize(shift_right(sum, AVG_SHIFT), DATA_W));
 average_ena <= '1';
 
-data_r 	<= shift_left(resize(signed(data),AVG_W),SHIFT_IN);
-alpha_r <= shift_left(resize(unsigned(alpha),AVG_W),SHIFT_IN);
-alpha_m <= resize(alpha_max - unsigned(alpha), AVG_W);
+alpha_signed <= signed(alpha);
+alpha_m 	 <= alpha_max - alpha_signed;
+
+data_signed <= signed(data);
+
+sum_r 		<= resize(shift_right(sum, SUM_SHIFT), PROD_W - SUM_SHIFT);
+
+mult_data 	<= shift_left(resize(data_signed * alpha_signed, PROD_W), MULT_DATA_SHIFT);
+mult_sum 	<= shift_left(resize(sum_r * alpha_m, PROD_W), MULT_SUM_SHIFT);
 
 proc : PROCESS (clk)
 BEGIN
 	IF clk'EVENT AND clk = '1' THEN
 		IF init = '1' THEN 
-			ema	<= (OTHERS => '0');
+			sum	<= (OTHERS => '0');
 		ELSE
 
 			-- ema[n] =  ( alpha*16*x[n]*16 + [1-alpha]*ema[n-1] ) / 256
 
-			ema <= resize(shift_right(data_r * signed(alpha_r) + ema * signed(alpha_m), ALPHA_W), AVG_W);
-
+			sum <= mult_data + mult_sum;
 
 		END IF;
 	END IF;
